c77bdbe62a4bc30852a6aac45479f3b4254e5fdf
Rustamov-Javohir <javohirrustamov603@gmail.com>
2024-10-20 20:28:42 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadeTransitionDemo(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Animated Container')),
+        body: AnimatedContainerExample(),
+      ),
     );
   }
 }
 
-class FadeTransitionDemo extends StatefulWidget {
+class AnimatedContainerExample extends StatefulWidget {
   @override
-  _FadeTransitionDemoState createState() => _FadeTransitionDemoState();
+  _AnimatedContainerExampleState createState() =>
+      _AnimatedContainerExampleState();
 }
 
-class _FadeTransitionDemoState extends State<FadeTransitionDemo>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
-
-    _controller.forward(); // animatsiyani ishga tushiradi
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('FadeTransition Demo'),
-      ),
-      body: Center(
-        child: FadeTransition(
-          opacity: _animation,
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
-            child: Center(
-              child: Text(
-                'Hello, Flutter!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _isExpanded = !_isExpanded;
+          });
+        },
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          width: _isExpanded ? 400 : 200,
+          height: 100,
+          color: _isExpanded ? Colors.blue : Colors.red, // Rang
+          alignment: Alignment.center,
+          child: Text(
+            'Menga Bos!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
             ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            if (_controller.isCompleted) {
-              _controller.reverse(); // orqaga animatsiya
-            } else {
-              _controller.forward(); // oldinga animatsiya
-            }
-          });
-        },
-        child: Icon(Icons.play_arrow),
       ),
     );
   }
