e616d05c63f2b7cf9772811446be957d0f111d35
Rustamov-Javohir <javohirrustamov603@gmail.com>
2024-10-20 21:50:39 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Simple Animated Container')),
-        body: AnimatedContainerExample(),
+        appBar: AppBar(
+          title: Text('DropdownButton Misoli'),
+        ),
+        body: Center(
+          child: DropdownExample(),
+        ),
       ),
     );
   }
 }
 
-class AnimatedContainerExample extends StatefulWidget {
+class DropdownExample extends StatefulWidget {
   @override
-  _AnimatedContainerExampleState createState() =>
-      _AnimatedContainerExampleState();
+  _DropdownExampleState createState() => _DropdownExampleState();
 }
 
-class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
-  bool _isExpanded = false; // Kenglik holati
+class _DropdownExampleState extends State<DropdownExample> {
+  String? _selectedValue;
+
+  // Dropdown uchun ro'yxat
+  final List<String> _options = ['Birinchi', 'Ikkinchi', 'Uchinchi'];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            _isExpanded = !_isExpanded; // Kenglikni o'zgartirish
-          });
-        },
-        child: AnimatedContainer(
-          duration: Duration(seconds: 1), // Animatsiya vaqti
-          width: _isExpanded ? 200 : 100, // Kenglik
-          height: 100, // Balandlik
-          color: _isExpanded ? Colors.blue : Colors.red, // Rang
-          alignment: Alignment.center,
-          child: Text(
-            'Tap Me!',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        DropdownButton<String>(
+          value: _selectedValue,
+          hint: Text('Biror narsani tanlang'),
+          onChanged: (String? newValue) {
+            setState(() {
+              _selectedValue = newValue;
+            });
+          },
+          items: _options.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
-      ),
+        SizedBox(height: 20),
+        Text(_selectedValue == null
+            ? 'Hech narsa tanlanmagan'
+            : 'Tanlangan: $_selectedValue'),
+      ],
     );
   }
 }
